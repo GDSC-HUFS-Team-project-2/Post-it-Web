bff42379fa853d111d67e3a9e2a7febcac6d6ef3
SarahParkSehyun <101391544+SarahParkSehyun@users.noreply.github.com>
2023-02-26 23:32:01 +0900

    Add files via upload
    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import styles from "./Login.module.css";
+import homestyles from "./Home.module.css"
+import ss from "./SignUpSuccess.module.css"
+import SignUp from './SignUp';
+import Home from './Home';
+import {
+  BrowserRouter,
+ Routes,
+ Route,
+ Link
+}from 'react-router-dom'
 
-function App() {
+
+
+
+
+function SignUpSuccess(){
+  return(
+    <div className={ss.threehalf}>
+      <div className={ss.yellowBackground}></div>
+      <div className={ss.right}>
+        <div className={ss.top}>
+          <h1 className={ss.h1AfterLogin}>회원가입</h1>
+        </div>
+        <div className={ss.center}>
+          <h2 className={ss.noteintroduce}>회원가입 완료!</h2>
+        </div>
+        <div className={ss.under}>
+          <a href='Login.html'><input type="submit" className={ss.button} value="로그인 하러가기"></input></a>
+        </div>
+      </div>
+      <div className={ss.yellowBackground}></div> 
+    </div>
+  )
+}
+
+  
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+<BrowserRouter>
+    <div>
+      <header>
+        <Home>
+          <Routes>
+        <Route exact path="/" element={SignUp}/>
+        <Route path="/signup" element={SignUp}/>
+      </Routes>
+          </Home>
       </header>
     </div>
+</BrowserRouter>
   );
 }
 
-export default App;
+
