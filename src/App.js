db2d7e9970a573cac4c1df8d6f31dd5dde8b06a9
Dawon00 <da6921@naver.com>
2023-03-03 12:01:32 +0900

    fix: App.js 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Routes, Route, useNavigate, Outlet, Link } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  Outlet,
+  Link,
+} from "react-router-dom";
 import { lazy, Suspense } from "react";
-import React from 'react';
+import React from "react";
 import styles from "./Login.module.css";
-import homestyles from "./Home.module.css"
-import ss from "./SignUpSuccess.module.css"
-import SignUp from './SignUp';
-import Home from './Home';
-
+import homestyles from "./Home.module.css";
+import ss from "./SignUpSuccess.module.css";
+import SignUp from "./SignUp";
+import Home from "./Home";
 
 const Note = lazy(() => import("./page/Note.js"));
 const Post = lazy(() => import("./page/Post.js"));
 
-
-
-function SignUpSuccess(){
-  return(
+function SignUpSuccess() {
+  return (
     <div className={ss.threehalf}>
       <div className={ss.yellowBackground}></div>
       <div className={ss.right}>
@@ -27,15 +31,20 @@ function SignUpSuccess(){
           <h2 className={ss.noteintroduce}>회원가입 완료!</h2>
         </div>
         <div className={ss.under}>
-          <a href='Login.html'><input type="submit" className={ss.button} value="로그인 하러가기"></input></a>
+          <a href="Login.html">
+            <input
+              type="submit"
+              className={ss.button}
+              value="로그인 하러가기"
+            ></input>
+          </a>
         </div>
       </div>
-      <div className={ss.yellowBackground}></div> 
+      <div className={ss.yellowBackground}></div>
     </div>
-  )
+  );
 }
 
-  
 export default function App() {
   return (
     <div className="App">
@@ -46,21 +55,6 @@ export default function App() {
           <Route path="*" element={<div>404</div>}></Route>
         </Routes>
       </Suspense>
-
     </div>
-<BrowserRouter>
-    <div>
-      <header>
-        <Home>
-          <Routes>
-        <Route exact path="/" element={SignUp}/>
-        <Route path="/signup" element={SignUp}/>
-      </Routes>
-          </Home>
-      </header>
-</div>
-</BrowserRouter>
   );
 }
-
-
