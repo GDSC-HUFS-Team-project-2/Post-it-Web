7c2f54ae797b59d940714c659cd996f1c996c5f7
박세현 <sarah1918@naver.com>
2023-03-06 22:25:48 +0900

    Home.js 수정

--- src/App.js
+++ src/App.js
@@ -10,41 +10,15 @@ import {
 } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import React from "react";
-import styles from "./Login.module.css";
-import homestyles from "./Home.module.css";
-import ss from "./SignUpSuccess.module.css";
-import SignUp from "./SignUp";
+
+
 
 
 const Note = lazy(() => import("./page/Note.js"));
 const Post = lazy(() => import("./page/Post.js"));
 const Home = lazy(() => import("./page/Home.js"));
+const SignUp = lazy(() => import("./page/SignUp.js"));
 
-function SignUpSuccess() {
-  return (
-    <div className={ss.threehalf}>
-      <div className={ss.yellowBackground}></div>
-      <div className={ss.right}>
-        <div className={ss.top}>
-          <h1 className={ss.h1AfterLogin}>회원가입</h1>
-        </div>
-        <div className={ss.center}>
-          <h2 className={ss.noteintroduce}>회원가입 완료!</h2>
-        </div>
-        <div className={ss.under}>
-          <a href="Login.html">
-            <input
-              type="submit"
-              className={ss.button}
-              value="로그인 하러가기"
-            ></input>
-          </a>
-        </div>
-      </div>
-      <div className={ss.yellowBackground}></div>
-    </div>
-  );
-}
 
 export default function App() {
   return (
@@ -52,7 +26,9 @@ export default function App() {
       <Suspense fallback={<div>loading...</div>}>
         <Routes>
           <Route path="/note/:home" element={<Home />}></Route>
+          <Route path="/note/:user_login" element={<Login />}></Route>
           <Route path="/note/:user_id" element={<Note />}></Route>
+          <Route path="/note/:user_signup" element={<SignUp />}></Route>
           <Route path="/note/:note_id/write" element={<Post />}></Route>
           <Route path="*" element={<div>404</div>}></Route>
         </Routes>
---
+++ src/page/Home.js
@@ -0,0 +1,62 @@
+import homestyles from "../Home.module.css"
+import { useNavigate } from "react-router-dom";
+import {Link} from 'react-router-dom'
+import styled, { css } from "styled-components";
+
+
+const Button = styled.button`
+  font-size: 200%;
+  background-color: #FFE27A;
+  color: black;
+  width: 100%;
+  height: 7vh;
+  border-radius: 10px;
+  border-style: hidden;
+  transition: 1s;
+  font-family: "godic";
+  font-weight: 600;
+  &:hover {
+  background: #ffc800;
+  height: 10vh;
+  }
+`;
+
+export default function Home(){
+  let navigate=useNavigate();
+
+
+  return(
+    <div className={homestyles.threehalf}>
+      <div className={homestyles.yellowBackground}></div>
+      <div className={homestyles.right}>
+        <div className={homestyles.top}>
+          <img src="img/post_it.png" height="200px" width="200px" />
+          <h1 className={homestyles.h1}>Post - it</h1>
+        </div>
+        <div className={homestyles.center}>
+        </div>
+        <div className={homestyles.under}>
+          <Button
+              onClick={() => {
+                navigate("/note/:user_login");
+              }}
+            >
+              로그인
+            </Button>
+        </div>
+        <div className={homestyles.under}>
+          <Button
+              onClick={() => {
+                navigate("/note/:user_signup");
+              }}
+            >
+              회원가입
+            </Button>
+          <p><Link to="/signup">개발자 소개</Link></p>
+        </div>
+
+      </div>
+      <div className={homestyles.yellowBackground}></div> 
+    </div>
+  )
+}
